40f71ca0caedcc62cbc513217a25a615e3045d40
Daniel <dev.dan.horrigan@gmail.com>
2022-06-22 11:02:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sci-calc: A cli-based scientific calculator
---
+++ src/main/java/org/dh/App.java
@@ -0,0 +1,13 @@
+package org.dh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
